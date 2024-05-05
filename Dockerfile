915ba8804c53f3cab189d5086136c3f0406e534a
Jin Choi <choi273@wisc.edu>
2024-05-05 02:58:16 -0500

    edited html and routes.py

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Start with a base Python image
+FROM python:3.9-slim
+
+# Set the working directory inside the container to /app
+WORKDIR /app
+
+# Copy the requirements file into the container
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of your application code
+COPY . .
+
+# Expose the port Gunicorn will run on
+EXPOSE 8000
+
+# Finally, run Gunicorn with the WSGI application
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "run:app"]
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,44 @@
 from flask import Blueprint, render_template, request
 from .utilities.text_utils import load_text, find_sentences_containing_word
+import nltk
+from nltk.tokenize import sent_tokenize
 
+nltk.download('punkt')
 # Create a Blueprint for the main routes
 main = Blueprint('main', __name__)
 
-@main.route('/', methods=['GET', 'POST'])
-def home():
-    word = request.args.get('word', '')
-    sentences = []
-    if word:
-        text = load_text('Germania.txt')
-        sentences = find_sentences_containing_word(text, word)
-    return render_template('index.html', sentences=sentences, word=word)
+def load_text(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
+text = load_text('Germania.txt')
+
+def find_sentences_containing_word(text, word):
+    sentences = sent_tokenize(text, language='english')
+    return [sentence for sentence in sentences if word.lower() in sentence.lower()]
 
 @main.route('/tribes')
 def tribes():
     return render_template('tribes.html')
+
+@main.route('/search', methods=['GET'])
+def search():
+    word = request.args.get('word', '') 
+    if text and word:
+        sentences = find_sentences_containing_word(text, word)
+    else:
+        sentences = []
+    return render_template('index.html', sentences=sentences, word=word)
+
+
+@main.route('/', methods=['GET', 'POST'])
+def home():
+    word = ""
+    if request.method == 'POST':
+        word = request.form.get('word', '')
+        if text and word:  
+            sentences = find_sentences_containing_word(text, word)
+            return render_template('index.html', sentences=sentences, word=word)
+    return render_template('index.html', sentences=[], word=word)
