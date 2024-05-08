ea811cb6c1d9284e69f310796b448b46c22ace2d
Jin Choi <choi273@wisc.edu>
2024-05-08 17:37:48 -0500

    run.py edited

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 # import os
 from app import create_app
 
-# app = create_app()
+app = create_app()
 
 if __name__ == '__main__':
     # port = int(os.environ.get('PORT', 8000))
     # app.run(host='0.0.0.0', port=port)v
-    app = create_app()
     app.run(debug=True)
 
 
