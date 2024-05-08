ed135d9a86f5ba2992ac36124dd2820a0c552ed9
Jin Choi <choi273@wisc.edu>
2024-05-08 17:30:32 -0500

    procfile edited

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
-import os
+# import os
 from app import create_app
 
-app = create_app
+# app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 8000))
+    # app.run(host='0.0.0.0', port=port)
+    app = create_app()
+    app.run(debug=True)
+
 
